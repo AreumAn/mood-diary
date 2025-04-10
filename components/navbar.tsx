7d2f15315eddec2193fec546335ab7a68a00a9d7
Areum An <areum.an.0928@gmail.com>
2025-04-09 23:48:44 -0400

    add home link to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,15 +4,17 @@ import { ThemeToggle } from "./theme-toggle";
 import { LanguageToggle } from "./language-toggle";
 import { useLanguage } from "@/lib/language-provider";
 import { t } from "@/lib/translations";
-
+import Link from 'next/link';
 export function Navbar() {
   const { language } = useLanguage();
   
   return (
-    <nav className="border-b border-border bg-background">
-      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 className="text-xl font-bold">{t("appName", language)}</h1>
-        <div className="flex items-center space-x-2">
+    <nav className='border-b border-border bg-background'>
+      <div className='container mx-auto px-4 py-3 flex justify-between items-center'>
+        <Link href='/'>
+          <h1 className='text-xl font-bold'>{t('appName', language)}</h1>
+        </Link>
+        <div className='flex items-center space-x-2'>
           <LanguageToggle />
           <ThemeToggle />
         </div>
